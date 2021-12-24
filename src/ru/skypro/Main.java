693fce46d034a6bbd74adcec8937411b421cd3ed
SerGGey <-1010>
2021-12-24 14:48:48 +0300

    первый комит

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -10,7 +10,22 @@ public class Main {
         float e = 1.23f;
         double f = 25.1356D;
 
-    }
+        float weightFighterOne = 78.2f;
+        float weightFighterTwo = 82.7f;
+
+        float totalWeight = weightFighterTwo + weightFighterOne;
+        System.out.println("Общий вес бойцов " + totalWeight + "кг!");
+
+        float differenceWeight = weightFighterTwo - weightFighterOne;
+        System.out.println("Разница бойцов в весе составляет " + differenceWeight + "кг!");
+
+    }}
+
+
+
+
+
+
 
 
 
