31ec13b0ed485cede738773dacb7708f73bea102
SerGGey <-1010>
2021-12-24 14:34:45 +0300

    первый комит

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,27 @@
+package ru.skypro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte a = 1;
+        short b = 235;
+        int c = 5555;
+        long d = 123L;
+        float e = 1.23f;
+        double f = 25.1356D;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
